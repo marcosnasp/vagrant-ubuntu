f8a116f4f965f7c030ed07e7f3de3239c4038f71
Marcos Portela <marcosnasp@gmail.com>
2019-02-12 13:50:21 -0300

    Update provision.sh
    Fixing SpringToolSuite.desktop

--- provisioning/shell/provision.sh
+++ provisioning/shell/provision.sh
@@ -40,10 +40,10 @@ apt-packages-update
 apt-packages-install \
     tilix             
 
-#wget -c http://download.springsource.com/release/STS4/4.1.1.RELEASE/dist/e4.10/spring-tool-suite-4-4.1.1.RELEASE-e4.10.0-linux.gtk.x86_64.tar.gz -O SpringToolSuite4.tar.gz
-#sudo tar -zxvf SpringToolSuite4.tar.gz -C /opt/
-#cd ~/.local/share/applications
-#echo -e '[Desktop Entry]\nCategories=Development;IDE;Java;\nComment[pt_BR]=Spring Tool Suite 4\nComment=Spring Tool Suite 4\nExec=/opt/sts-4.1.1.RELEASE/SpringToolSuite4\nGenericName[pt_BR]=\nGenericName=\nIcon=/opt/sts-4.1.1.RELEASE/icon.xpm\nMimeType=\nName[pt_BR]=Spring Tool Suite 4\nName=STS4\nPath=/opt/sts-4.1.1.RELEASE\nStartupNotify=true\nStartupWMClass=STS4\nTerminal=false\nTerminalOptions=\nType=Application\nX-DBUS-ServiceName=\nX-DBUS-StartupType=\nX-KDE-SubstituteUID=false\nX-KDE-Username=' | sudo tee ~/.local/share/applications/SpringToolSuite4.desktop
+wget -c http://download.springsource.com/release/STS4/4.1.1.RELEASE/dist/e4.10/spring-tool-suite-4-4.1.1.RELEASE-e4.10.0-linux.gtk.x86_64.tar.gz -O SpringToolSuite4.tar.gz
+sudo tar -zxvf SpringToolSuite4.tar.gz -C /opt/
+touch /usr/share/applications/SpringToolSuite4.desktop
+echo -e '[Desktop Entry]\nCategories=Development;IDE;Java;\nComment[pt_BR]=Spring Tool Suite 4\nComment=Spring Tool Suite 4\nExec=/opt/sts-4.1.1.RELEASE/SpringToolSuite4\nGenericName[pt_BR]=\nGenericName=\nIcon=/opt/sts-4.1.1.RELEASE/icon.xpm\nMimeType=\nName[pt_BR]=Spring Tool Suite 4\nName=STS4\nPath=/opt/sts-4.1.1.RELEASE\nStartupNotify=true\nStartupWMClass=STS4\nTerminal=false\nTerminalOptions=\nType=Application\nX-DBUS-ServiceName=\nX-DBUS-StartupType=\nX-KDE-SubstituteUID=false\nX-KDE-Username=' | sudo tee /usr/share/applications/SpringToolSuite4.desktop
 
 # Sdkman
 curl -s "https://get.sdkman.io" | bash
